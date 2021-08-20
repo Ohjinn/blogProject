26d4e9aaf5b5f65f5262644e7a523c32c5012b7c
Ohjinn <hjin9445@gamil.com>
2021-08-21 02:56:26 +0900

    210820

--- blog/src/main/java/com/cos/blog/controller/UserController.java
+++ blog/src/main/java/com/cos/blog/controller/UserController.java
@@ -1,5 +1,10 @@
 package com.cos.blog.controller;
 
+import com.cos.blog.model.KakaoProfile;
+import com.cos.blog.model.OAuthToken;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -59,7 +64,51 @@ public class UserController {
                 kakaoTokenRequest,
                 String.class
         );
-        return "카카오 토큰 요청 완료 + 토큰 요청에 대한 응답" + response;
+
+        //Gson, Json Simple, ObjectMapper
+        ObjectMapper objectMapper = new ObjectMapper();
+        OAuthToken oauthToken = null;
+        try{
+            oauthToken = objectMapper.readValue(response.getBody(), OAuthToken.class);
+        }catch (JsonMappingException e){
+            e.printStackTrace();
+        }catch (JsonProcessingException e){
+            e.printStackTrace();
+        }
+
+        RestTemplate rt2 = new RestTemplate();
+
+        //HttpHeader 오브젝트 생성
+        HttpHeaders headers2 = new HttpHeaders();
+        headers2.add("Authorization", "Bearer " +oauthToken.getAccess_token());
+        headers2.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+
+        //HttpHeader와 HttpBody를 하나의 오브젝트에 담기
+        HttpEntity<MultiValueMap<String, String>> kakaoProfileRequest2 =
+                new HttpEntity<>(headers2);
+
+        //Http 요청하기 - POST방식으로 - 그리고 response 변수의 응답 받음
+        ResponseEntity<String> response2 = rt2.exchange(
+                "https://kapi.kakao.com/v2/user/me",
+                HttpMethod.POST,
+                kakaoProfileRequest2,
+                String.class
+        );
+
+        ObjectMapper objectMapper2 = new ObjectMapper();
+        KakaoProfile kakaoProfile = null;
+        try{
+            kakaoProfile = objectMapper2.readValue(response2.getBody(), KakaoProfile.class);
+        }catch (JsonMappingException e){
+            e.printStackTrace();
+        }catch (JsonProcessingException e){
+            e.printStackTrace();
+        }
+
+        System.out.println("카카오 아이디(번호) : " + kakaoProfile.getId());
+        System.out.println("카카오 이메일(번호) : " + kakaoProfile.getKakaoAccount().getEmail());
+
+        return response2.getBody();
     }
 
     @GetMapping("/user/updateForm")
---
+++ blog/src/main/java/com/cos/blog/model/KakaoProfile.java
@@ -0,0 +1,50 @@
+package com.cos.blog.model;
+
+import lombok.Data;
+
+@Data
+public class KakaoProfile {
+
+    public Integer id;
+    public String connectedAt;
+    public Properties properties;
+    public KakaoAccount kakaoAccount;
+
+    @Data
+    class Properties {
+
+        public String nickname;
+        public String profileImage;
+        public String thumbnailImage;
+
+    }
+
+    @Data
+    public class KakaoAccount {
+
+        public Boolean profileNicknameNeedsAgreement;
+        public Boolean profileImageNeedsAgreement;
+        public Profile profile;
+        public Boolean hasEmail;
+        public Boolean emailNeedsAgreement;
+        public Boolean isEmailValid;
+        public Boolean isEmailVerified;
+        public String email;
+
+        @Data
+        class Profile {
+
+            public String nickname;
+            public String thumbnailImageUrl;
+            public String profileImageUrl;
+            public Boolean isDefaultImage;
+
+        }
+    }
+}
+
+
+
+
+
+
